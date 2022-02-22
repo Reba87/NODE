4e44208bf680124fafa7c887bb78ebea61307112
Rafael Bravo <rafaelbaponte87@gmail.com>
2022-02-22 15:52:07 +0100

    tercera subida modificacion de archivos

--- ejemplosServidor.js
+++ ejemplosServidor.js
@@ -12,3 +12,17 @@ const server = http.createServer(function(request, response) {
 })
 
 server.listen(3000);
+
+
+/* 
+
+
+ LOS METODOS SON EXCLUSIVOS DE LAS REQUEST 
+Los metodos(method) mas usados y habituales son los siguientes: 
+- "GET": Para obtener informacion del servidor, cuando el cliente hace
+una peticion, EL METODO GET NUNCA TIENE BODY.
+- "POST": Para añadir informacion, ejemplo cuando creamos un usuario, cuando rellenamos un formulario, etc. 
+- "PUT": Para realizar algun cambio en la informacion que tenemos, ejemplo cuando cambiamos una contraseña,
+cuando cambiamos una foto de perfil, etc.
+- "DELETE": Para eliminar datos, ejem borrar el perfil o cuenta de un usuario 
+*/
